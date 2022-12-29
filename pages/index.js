69e88374d0358e7e1ec02ed25af9855415b17020
Gayoung Yoo <olivia378@naver.com>
2022-12-29 14:36:10 +0900

    1st

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <Layout title="home">
+      <h1 className="text-3xl text-blue-500">next ecommerce</h1>
+      <p>Author: Gayoung Yoo</p>
+    </Layout>
+  )
+}
